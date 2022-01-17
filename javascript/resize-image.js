02f40602e2df9801a7fd9697fee3d81c08c49bae
OldFrenchGuy <jl.varennes@orange.fr>
2022-01-17 18:04:03 +0100

    Use of pointer capture

--- javascript/resize-image.js
+++ javascript/resize-image.js
@@ -7,15 +7,19 @@ class ResizeImageElement extends HTMLImageElement {
         super();
     }
 
+    static get observedAttributes() { return ['x']; }
+
+    attributeChangedCallback() {
+        this.positionResizeHandlers();
+    }
+
     connectedCallback() {
         console.log("UImage connectedCallback ");
-        // Init style and tabindex attriute in order to get the onblur event
+        // Init style and tabindex attribute in order to get the onblur event
         this.style.cursor = 'pointer';
         this.setAttribute('tabindex', 1000);
-        // set click and blur events
-        this.$onclick = this.onclick;
+        // set image click and blur events
         this.onclick = (e) => this.click(e);
-        this.$onblur = this.onblur;
         this.onblur = (e) => this.blur(e);
         // get container max dimensions
         var container = this.closest('li,div,td');
@@ -28,128 +32,96 @@ class ResizeImageElement extends HTMLImageElement {
         // cleanup image element
         this.style.cursor = '';
         this.removeAttribute('tabindex');
-        // remove any added element and/or events, if needed
-        this.blur();
-        // restore document mouse (and up...) event, needed ???
-        // if (this.$oldOnMouseMoveHandler) document.onmousemove = this.$oldOnMouseMoveHandler;
-        // this.$oldOnMouseMoveHandler = null;
+        this.removeResizeHandlers();
+    }
 
-        // restore events
-        this.onclick = this.$onclick;
-        this.onblur = this.$onblur;
+    addResizeHandlers() {
+        this.$rightHandler = document.createElement('div');
+        this.$rightHandler.setAttribute('style', 'width: 10px; height: 10px; border: 1px solid black;  background-color: white; position: absolute;');
+        this.$rightHandler.style.cursor = 'grab';
+        this.$rightHandler.dataset.resize = 'right';
+
+        this.$leftHandler = this.$rightHandler.cloneNode();
+        this.$leftHandler.dataset.resize = 'left';
+
+        this.positionResizeHandlers();
+
+        this.parentNode.insertBefore(this.$rightHandler, this.nextSibling);
+        this.parentNode.insertBefore(this.$leftHandler, this.nextSibling);
+    }
+
+    positionResizeHandlers() {
+        this.$rightHandler.style.left = (window.scrollX + this.x + this.width - 8) + 'px';
+        this.$leftHandler.style.left = (window.scrollX + this.x - 1) + 'px';
+        this.$rightHandler.style.top = this.$leftHandler.style.top = (window.scrollY + this.y + this.height - 8) + 'px';
+    }
+
+    removeResizeHandlers() {
+        if (this.$rightHandler) this.$rightHandler.remove();
+        if (this.$leftHandler) this.$leftHandler.remove();
     }
 
     click(e) {
-        // set cursor
         this.style.cursor = 'move';
+        this.$ratio = this.width / this.height;
 
-        // add the resize handlers
-        this.$div = document.createElement('div');
-        this.$div.setAttribute('style', 'width: 10px; height: 10px; border: 1px solid black;  background-color: white; position: absolute;');
-        this.$div2 = this.$div.cloneNode();
-
-        this.$div.style.left = (window.scrollX + this.x + this.width - 8) + 'px';
-        this.$div.style.top = (window.scrollY + this.y + this.height - 8) + 'px';
-        this.$div2.style.left = (window.scrollX + this.x - 1) + 'px';
-        this.$div2.style.top = (window.scrollY + this.y + this.height - 8) + 'px';
-
-        this.$div.style.cursor = 'grab';
-        this.$div2.style.cursor = 'grab';
-
-
-        this.parentNode.insertBefore(this.$div, this.nextSibling);
-        this.parentNode.insertBefore(this.$div2, this.nextSibling);
-
-        // set resize mousedown event
-        this.$div.addEventListener('mousedown', (e) => this.resizeMouseDown(e, 'right'));
-        this.$div2.addEventListener('mousedown', (e) => this.resizeMouseDown(e, 'left'));
+        // create and add the resize handlers, and set mousedown event on them
+        this.addResizeHandlers();
+        this.$rightHandler.onpointerdown = (e) => { this.resizeMouseDown(e) };
+        this.$leftHandler.onpointerdown = (e) => { this.resizeMouseDown(e) };
     }
 
     blur(e) {
-        // restore cursor
         this.style.cursor = 'pointer';
-        // Delete the resize handlers
-        if (this.$div) this.$div.remove();
-        if (this.$div2) this.$div2.remove();
+        this.removeResizeHandlers();
     }
 
     resizeMouseDown(e, leftright) {
-        console.log('resizeMouseDown');
+        this.$rightHandler.style.cursor = this.$leftHandler.style.cursor = 'grabbing';
         // set resize variables
-        this.$resizing = leftright;
-        this.$x = this.x; this.$y = this.y;
-        this.$ex = e.x; this.$ey = e.y;
+        this.$x = this.x;
+        this.$ex = e.x;
         this.$oWidth = this.width;
-        this.$ratio = this.width / this.height;
-
-        this.$div.style.cursor = 'grabbing';
-        this.$div2.style.cursor = 'grabbing';
-        this.style.cursor = 'grabbing';
-
-        // set event handlers
-        this.$oldOnMouseMoveHandler = document.onmousemove;
-        document.onmousemove = (e) => this.mouseMove(e);
-        this.$oldOnMouseUpHandler = document.onmouseup;
-        document.onmouseup = (e) => this.mouseUp(e);
-
+        this.$xw = this.x + this.width;
+        // set capture event handlers
+        e.target.onpointermove = (e) => { this.resizeMouseMove(e) };
+        e.target.onpointerup = (e) => { this.resizeMouseUp(e) };
+        e.target.setPointerCapture(e.pointerId);
+        // avoid to move image instead
         e.stopPropagation(); e.preventDefault();
     }
 
-    mouseUp(e) {
-        if (this.$resizing) {
-            // stop resizing
-            this.$resizing = false;
-
-            // restore document mousemove event
-            document.onmousemove = this.$oldOnMouseMoveHandler;
-            this.$oldOnMouseMoveHandler = null;
-            document.onmouseup = this.$oldOnMouseUpHandler;
-            this.$oldOnMouseUpHandler = null;
-
-            this.$div.style.cursor = 'grab';
-            this.$div2.style.cursor = 'grab';
-            this.style.cursor = 'move';
-
-            e.stopPropagation(); e.preventDefault();
-        }
+    resizeMouseUp(e) {
+        this.$rightHandler.style.cursor = this.$leftHandler.style.cursor = 'grab';
+        // remove capture events
+        e.target.onpointermove = e.target.onpointerup = null;
+        e.target.releasePointerCapture(e.pointerId);
     }
 
-    mouseMove(e) {
-        if (this.$resizing) {
-            // resize 
-            let newWidth;
-            let coef = this.x != this.$x ? 2 : 1; // in case of centered image
-            if (this.$resizing == 'right') {
-                newWidth = this.$oWidth + (e.x - this.$ex) * coef;
-            } else {
-                newWidth = this.$oWidth + (this.$ex - e.x) * coef;
-            }
+    resizeMouseMove(e) {
+        let coef = this.x != this.$x && this.x + this.width != this.$xw ? 2 : 1; // in case of centered image
+        let delta = e.target.dataset.resize == 'right' ? e.x - this.$ex : this.$ex - e.x;
+        let newWidth = this.$oWidth + delta * coef;
+        this.resizeImage(newWidth);
+    }
 
-            if (newWidth >= 20) {
-                let newHeight = newWidth / this.$ratio;
-                if (newWidth <= this.$parentWidth && newHeight <= this.$parentHeight) {
-                    // resize image
-                    this.width = newWidth;
-                    this.height = newHeight;
-                    // re-pos resize handlers
-                    this.$div.style.left = (window.scrollX + this.x + this.width - 8) + 'px';
-                    this.$div.style.top = (window.scrollY + this.y + this.height - 8) + 'px';
-                    this.$div2.style.left = (window.scrollX + this.x - 1) + 'px';
-                    this.$div2.style.top = (window.scrollY + this.y + this.height - 8) + 'px';
-                }
+    resizeImage(newWidth) {
+        if (newWidth >= 20) {
+            let newHeight = newWidth / this.$ratio;
+            if (newWidth <= this.$parentWidth && newHeight <= this.$parentHeight) {
+                this.width = newWidth; this.height = newHeight;
+                this.positionResizeHandlers();
             }
-
-            e.stopPropagation(); e.preventDefault();
         }
     }
 }
 
 /**
- * Derived class to add some controls
+ * Derived class to add some basic controls
  */
 class CtlResizeImage extends ResizeImageElement {
 
-    static get observedAttributes() { return ['width', 'height']; }
+    static get observedAttributes() { return ['width', 'height', 'src']; }
 
     attributeChangedCallback() {
         this.showInfos();
@@ -158,23 +130,47 @@ class CtlResizeImage extends ResizeImageElement {
     showInfos() {
         document.getElementById('txtwidth').value = this.width;
         document.getElementById('txtheight').value = this.height;
+        document.getElementById('txtsrc').innerText = this.src;
     }
 
     click(e) {
         super.click(e);
         this.showInfos();
+        let ctls = document.querySelectorAll('.txtpercent');
+        ctls.forEach((el) => {
+            el.addEventListener('click', (e) => { this.resizePercentage(e) })
+        });
     }
 
-    mouseUp(e) {
-        super.mouseUp(e);
+    blur(e) {
+        if (e.target == this) return;
 
-        let parts = this.src.split('/');
-        let len = parts.length;
+        super.blur(e);
+        document.getElementById('txtwidth').value = '';
+        document.getElementById('txtheight').value = '';
+        document.getElementById('txtsrc').innerText = '';
+        document.getElementById('txtpercent').onclick = null;
+    }
+
+    resizeMouseUp(e) {
+        super.resizeMouseUp(e);
+        this.refreshImageResolution();
+    }
+
+    refreshImageResolution() {
+        const parts = this.src.split('/');
+        const len = parts.length;
         parts[len - 2] = this.width;
         parts[len - 1] = this.height;
         this.src = parts.join('/');
     }
 
+    resizePercentage(e) {
+        let percent = e.srcElement.dataset.percent;
+        super.resizeImage(this.width * percent);
+        this.refreshImageResolution();
+    }
 }
+
 window.customElements.define('resize-image', ResizeImageElement, { extends: 'img' });
 window.customElements.define('ctlresize-image', CtlResizeImage, { extends: 'img' });
